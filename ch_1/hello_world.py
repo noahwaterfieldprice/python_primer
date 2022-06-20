ee423481b33d534363df1e8db503d14a27009074
SEM-DEV, Inc <oualid909@gmail.com>
2022-06-20 23:24:30 +0300

    SyntaxError: Missing parentheses
    SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?

--- ch_1/hello_world.py
+++ ch_1/hello_world.py
@@ -1,8 +1,15 @@
 # Exercise 1.2
 # Author: Noah Waterfield Price
 
-print "Hello, World!"
 
+
+print ("Hello, World!")
+
+"""
+print "Hello, World!"
+    ^^^^^^^^^^^^^^^^^^^^^
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+"""
 """
 Sample run:
 python hello_world.py
